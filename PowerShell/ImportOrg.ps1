3f7190a1fa9b3a1b4733fb1953f8dab9b051689f
dklinger <d.klinger@global-concepts.de>
2015-10-15 17:23:46 +0200

    - Added script to trigger an organization Import

---
+++ PowerShell/ImportOrg.ps1
@@ -0,0 +1,51 @@
+﻿param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [string]$sqlName,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [string]$dbname,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [string]$srsUrl,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [string]$orgName,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [string]$serverUrl,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [string]$almServer,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [PSCredential]$cred
+)
+
+$session = New-PSSession -credential $cred -ComputerName $almServer
+$result = Invoke-Command -session $session -ScriptBlock { 
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [string]$sqlName,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [string]$dbname,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [string]$srsUrl,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [string]$orgName,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [string]$serverUrl,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [PSCredential]$cred
+    )
+
+    if (-not (Get-PSSnapin -Name Microsoft.Crm.PowerShell -ErrorAction SilentlyContinue))
+    {
+        Add-PSSnapin Microsoft.Crm.PowerShell
+        $RemoveSnapInWhenDone = $True
+    }
+
+    Import-CrmOrganization $sqlName $dbname $srsUrl $displayName $orgName "KeepExisting" -Credential $cred -DwsServerUrl $serverUrl
+
+    if($RemoveSnapInWhenDone)
+    {
+        Remove-PSSnapin Microsoft.Crm.PowerShell
+    }
+} -ArgumentList $sqlName, $dbname, $srsUrl, $orgName, $serverUrl, $cred
+
+Remove-PSSession -Id $session.Id
+
+return $true
