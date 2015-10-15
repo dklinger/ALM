2ab90f37bef3f6164e59a5e6cc4647f736238565
dklinger <d.klinger@global-concepts.de>
2015-10-15 17:24:32 +0200

    - Added Script to create new Feature and call Org-Copy-Script

---
+++ PowerShell/GitFlow/StartFeature.ps1
@@ -0,0 +1,21 @@
+﻿param(
+    [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+    [string]$FeatureName
+)
+
+try
+{
+    $FeatureName = $FeatureName -creplace '[^a-zA-Z0-9\-]', ''
+    $FeatureName = $FeatureName -replace '(.{30}).+','$1'
+
+    cd C:\Arbeit\Git-Repos\Heraeus_MSCRM
+
+    git flow feature start $FeatureName
+
+    cd C:\GC\DeveloperTools\SyncToClients\ALM\PowerShell
+    .\CreateNewOrgFromExistingOrg.ps1 $FeatureName
+}
+catch
+{
+    throw
+}
