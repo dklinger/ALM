bd54bd3cb5819db2a6af5cfd63df23c4a6bcb95b
dklinger <d.klinger@global-concepts.de>
2015-10-15 11:03:32 +0200

    - Added fallback if hardcoded credentials are wrong -

--- PowerShell/RestoreCrmDatabase.ps1
+++ PowerShell/RestoreCrmDatabase.ps1
@@ -7,56 +7,76 @@
         [string]$backupFileName
 )
 
-# Set SQL Server instance name
-#$sqlName= "VSDEV-SQL2012-1\MSCRMVOGEL2015"
-
-# Set new or existing databse name to restote backup
-#$dbname= "master_MSCRM"
-
-# Set the existing backup file path
-#$backupPath= "\\sgc-ls.gcn.lan\Freigabe\DK\ALM\develop.bak"
-
-#Load the required assemlies SMO and SmoExtended.
-[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMO") | Out-Null
-[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SmoExtended") | Out-Null 
-
-# Connect SQL Server.
-$sqlServer = New-Object ('Microsoft.SqlServer.Management.Smo.Server') $sqlName
-
-$sqlServer.ConnectionContext.LoginSecure = $false 
-$credential = get-Credential 
-$sqlServer.ConnectionContext.Login=$credential.UserName 
-$sqlServer.ConnectionContext.set_SecurePassword($credential.Password) 
-
-# Get Instance backup directory
-$backupPath= $sqlServer.BackupDirectory + "\" + $backupFileName
-Write-Host "backup file path:"$backupPath
-
-# Create SMo Restore object instance
-$dbRestore = new-object ("Microsoft.SqlServer.Management.Smo.Restore")
-
-# Set database and backup file path
-$dbRestore.Database = $dbname
-$dbRestore.Devices.AddDevice($backupPath, "File")
-
-# Set the databse file location
-$dbRestoreFile = new-object("Microsoft.SqlServer.Management.Smo.RelocateFile")
-$dbRestoreLog = new-object("Microsoft.SqlServer.Management.Smo.RelocateFile")
-$dbRestoreFile.LogicalFileName = "mscrm"
-$dbRestoreFile.PhysicalFileName = $sqlServer.Information.MasterDBPath + "\" + $dbRestore.Database + "_Data.mdf"
-$dbRestoreLog.LogicalFileName = "mscrm" + "_Log"
-$dbRestoreLog.PhysicalFileName = $sqlServer.Information.MasterDBLogPath + "\" + $dbRestore.Database + "_Log.ldf"
-$dbRestore.RelocateFiles.Add($dbRestoreFile)
-$dbRestore.RelocateFiles.Add($dbRestoreLog)
-
-# Call the SqlRestore mathod to complete restore database 
-try
+Function Restore([bool]$askForCredentials)
 {
-    $dbRestore.SqlRestore($sqlServer)
-    Write-Host "...SQL Database"$dbname" Restored Successfully..."
+    #Load the required assemlies SMO and SmoExtended.
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMO") | Out-Null
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SmoExtended") | Out-Null 
+
+    # Connect SQL Server.
+    $sqlServer = New-Object ('Microsoft.SqlServer.Management.Smo.Server') $sqlName
+
+    if($askForCredentials)
+    {
+        $credential = get-Credential 
+        $sqlServer.ConnectionContext.LoginSecure = $false 
+        $sqlServer.ConnectionContext.Login=$credential.UserName 
+        $sqlServer.ConnectionContext.set_SecurePassword($credential.Password) 
+    }
+    else
+    {
+        $sqlServer.ConnectionContext.LoginSecure = $false 
+        $sqlServer.ConnectionContext.Login = "sa"
+        $sqlServer.ConnectionContext.Password = "Ts08mx"
+    }
+
+    # Get Instance backup directory
+    $backupPath= $sqlServer.BackupDirectory + "\" + $backupFileName
+    Write-Host "backup file path:"$backupPath
+
+    # Create SMo Restore object instance
+    $dbRestore = new-object ("Microsoft.SqlServer.Management.Smo.Restore")
+
+    # Set database and backup file path
+    $dbRestore.Database = $dbname
+    $dbRestore.Devices.AddDevice($backupPath, "File")
+
+    # Set the databse file location
+    $dbRestoreFile = new-object("Microsoft.SqlServer.Management.Smo.RelocateFile")
+    $dbRestoreLog = new-object("Microsoft.SqlServer.Management.Smo.RelocateFile")
+    $dbRestoreFile.LogicalFileName = "mscrm"
+    $dbRestoreFile.PhysicalFileName = $sqlServer.Information.MasterDBPath + "\" + $dbRestore.Database + "_Data.mdf"
+    $dbRestoreLog.LogicalFileName = "mscrm" + "_Log"
+    $dbRestoreLog.PhysicalFileName = $sqlServer.Information.MasterDBLogPath + "\" + $dbRestore.Database + "_Log.ldf"
+    $dbRestore.RelocateFiles.Add($dbRestoreFile)
+    $dbRestore.RelocateFiles.Add($dbRestoreLog)
+
+    # Call the SqlRestore mathod to complete restore database 
+    try
+    {
+        $dbRestore.SqlRestore($sqlServer)
+        Write-Host "...SQL Database"$dbname" Restored Successfully..."
+    }
+    catch 
+    {
+        if($_.Exception.InnerException -match "Login failed")
+        {
+            Write-Host "Login failed. Retrying asking for credentials."
+            return $false
+        }
+        else
+        {
+            Write-Host $_.Exception.InnerException.Message
+            Write-Host $_.Exception.InnerException;
+            throw 
+        }
+    }
+
+    return $true
 }
-catch 
+
+$loginSuccessful = Restore
+while($loginSuccessful -eq $false)
 {
-    Write-Host $_.Exception.InnerException.InnerException.InnerException.Message
-    Write-Host $_.Exception.StackTrace
+    $loginSuccessful = Restore $true
 }
