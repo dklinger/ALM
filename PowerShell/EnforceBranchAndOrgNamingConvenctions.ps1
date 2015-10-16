efca66c48175f4fb765e965e22e54834b8ae2e30
dklinger <d.klinger@global-concepts.de>
2015-10-16 14:55:48 +0200

    - Branch and Org naming conventions outsourced

---
+++ PowerShell/EnforceBranchAndOrgNamingConvenctions.ps1
@@ -0,0 +1,9 @@
+﻿param(
+    [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+    [string]$orgOrBranchName
+)
+
+$orgOrBranchName = $orgOrBranchName -creplace '[^a-zA-Z0-9\-]', ''
+$orgOrBranchName = $orgOrBranchName -replace '(.{30}).+','$1'
+
+return $orgOrBranchName
